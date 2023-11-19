2c222791c3bb4bd859ef6cb9c933c52452d9527d
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-19 16:04:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+let red= document.getElementById("browser-info");
+document.addEventListener("DOMContentLoaded", () => {
+    let userAgent = navigator.appVersion;
+    let browserName = navigator.appName;
+    let information = `You are using ${browserName} version ${userAgent}.`;
+
+    
+    if (red) {
+        red.textContent = information;
+    } else {
+        console.error("Element with id 'browserinfo' not found.");
+    }
+});
